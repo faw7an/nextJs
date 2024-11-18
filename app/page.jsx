86b31b66985a0fc82cee2eddeb7eb747e927d6fb
Fauzan Said <fauzdasoodais@gmail.com>
2024-11-18 16:20:11 +0300

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='w-full flex-center flex-col'>
+        <h1 className='head_text text-center'>
+            DIscover & Share 
+            <br className='max-md:hidden' />
+            <span className='orange_gradient text-center'>AI-Powered Prompts</span>
+        </h1>
+    </section>
+  )
+}
+
+export default Home
